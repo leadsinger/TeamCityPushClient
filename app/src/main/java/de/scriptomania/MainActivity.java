33e2b2efb75d6e12af5169ae79e3b70ef3304e5d
leadsinger <andre.poschwald@gmail.com>
2015-05-17 18:21:43 +0200

    - initial commit

---
+++ app/src/main/java/de/scriptomania/MainActivity.java
@@ -0,0 +1,114 @@
+package de.scriptomania;
+
+import android.content.Context;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import org.jboss.aerogear.android.core.Callback;
+import org.jboss.aerogear.android.unifiedpush.MessageHandler;
+import org.jboss.aerogear.android.unifiedpush.PushRegistrar;
+import org.jboss.aerogear.android.unifiedpush.RegistrarManager;
+import org.jboss.aerogear.android.unifiedpush.gcm.AeroGearGCMPushConfiguration;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+
+public class MainActivity extends AppCompatActivity implements MessageHandler {
+
+    private PushConfiguration aerogearPushConfiguration;
+    private static final String REGISTRAR_ID = "teamCityPushService";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        aerogearPushConfiguration = new PushConfiguration();
+        try {
+            RegistrarManager.config(REGISTRAR_ID, AeroGearGCMPushConfiguration.class)
+                    .setPushServerURI(new URI(aerogearPushConfiguration.getServerURL()))
+                    .addSenderId(aerogearPushConfiguration.getSenderID())
+                    .setVariantID(aerogearPushConfiguration.getVariantID())
+                    .setSecret(aerogearPushConfiguration.getSecret())
+                    .asRegistrar();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+
+        PushRegistrar registrar = RegistrarManager.getRegistrar(REGISTRAR_ID);
+        registrar.register(getApplicationContext(), new Callback<Void>() {
+
+            @Override
+            public void onSuccess(Void ignore) {
+                // Seems your device was registered in Unified Push Server
+                Toast.makeText(getApplicationContext(), "Successfull registered", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(Exception exception) {
+                Toast.makeText(getApplicationContext(), "Failed to register", Toast.LENGTH_SHORT).show();
+                Log.e("TeamCityPushClient", exception.getMessage(), exception);
+            }
+
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        RegistrarManager.registerMainThreadHandler(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        RegistrarManager.unregisterMainThreadHandler(this);
+    }
+
+    @Override
+    public void onDeleteMessage(Context context, Bundle bundle) {
+        // ignore
+    }
+
+    @Override
+    public void onMessage(Context context, Bundle bundle) {
+        Toast.makeText(this, bundle.getString("alert"), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onError() {
+        // ignore
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+
+
+}
---
+++ app/src/main/java/de/scriptomania/PushConfiguration.java
@@ -0,0 +1,58 @@
+package de.scriptomania;
+
+/**
+ * Created by leadsinger_mac_mini on 16/05/15.
+ */
+public class PushConfiguration {
+
+    private String senderID = "525825801218";
+    private String variantID = "256fde38-fa71-4fe7-8c42-bd2148186004";
+    private String secret = "d4b3f8ce-c1a3-442c-a22f-e27cebb64a36";
+    private String serverURL = "https://aerogear-scriptomania.rhcloud.com/ag-push/";
+
+    public PushConfiguration() {
+
+    }
+
+    public PushConfiguration(String senderID, String variantID, String secret) {
+        this.senderID = senderID;
+        this.variantID = variantID;
+        this.secret = secret;
+    }
+
+    public void initializeFromSettings() {
+        // TODO
+    }
+
+    public String getSenderID() {
+        return senderID;
+    }
+
+    public void setSenderID(String senderID) {
+        this.senderID = senderID;
+    }
+
+    public String getVariantID() {
+        return variantID;
+    }
+
+    public void setVariantID(String variantID) {
+        this.variantID = variantID;
+    }
+
+    public String getSecret() {
+        return secret;
+    }
+
+    public void setSecret(String secret) {
+        this.secret = secret;
+    }
+
+    public String getServerURL() {
+        return serverURL;
+    }
+
+    public void setServerURL(String serverURL) {
+        this.serverURL = serverURL;
+    }
+}
